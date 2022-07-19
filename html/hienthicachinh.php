fbd72e592aa1e4d33b1747de36b0e5270584c061
MaiXuanCuong <maixuancuong2906@gmail.com>
2022-07-19 15:30:57 +0700

    testtest

---
+++ html/hienthicachinh.php
@@ -0,0 +1,24 @@
+<?php
+ for ($i = 0; $i < 3 ; $i++) {
+    for ($j = 0; $j < 7 ; $j++) {
+        echo "* ";
+    };
+    echo " <br>";
+ };
+ echo " <br>";
+ for ($i = 0; $i < 5 ; $i++) {
+    for ($j = 0; $j <= $i ; $j++) {
+        echo "* ";
+    };
+    echo " <br>";
+ };
+ echo " <br>";
+ for ($i = 0; $i < 5 ; $i++) {
+    for ($j = 5; $j > $i ; $j--) {
+        echo "* ";
+    };
+    echo " <br>";
+ };
+
+
+
