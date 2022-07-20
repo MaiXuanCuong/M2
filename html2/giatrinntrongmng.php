ef1080c0e54f11d4f092bb928481c8f4345e4a4c
MaiXuanCuong <maixuancuong2906@gmail.com>
2022-07-20 20:28:48 +0700

    testtest

---
+++ html2/giatrinntrongmng.php
@@ -0,0 +1,20 @@
+<?php
+function giatrinhonhat($a){
+    $songuyen1 = $a;
+    $min= $songuyen1[0];
+    $index =0;
+    foreach ($songuyen1 as $key => $value){
+       if ($value < $min){
+        $min=$value;
+        $index = $key;
+       }
+    }
+    echo '<pre>';
+    print_r($songuyen1);
+    echo '</pre>';
+    echo "<br>";
+    echo 'Số nhỏ nhất là : ' .$min." tại vị trí ".($index+1);
+}
+$songuyen = [3,3,1,4,46,86,9];
+timgiatrinhonhat($songuyen);
+?>
