bb8c5065e84e4523304df8de54bd93f9a07ca339
MaiXuanCuong <maixuancuong2906@gmail.com>
2022-07-24 15:00:32 +0700

    testtest

--- Baitap/html4/quanlynhansu.php
+++ Baitap/html4/quanlynhansu.php
@@ -1,76 +1,5 @@
 <?php
-function loadRegistrations($fileName){
-    $jsonData = file_get_contents($fileName);
-    return json_decode($jsonData, true);
-}
-function saveDataJSON($fileName, $Ho,$Ten, $NgaySinh, $DiaChi, $ViTriCongViec){
-    try{
-        $contact = array(
-            'ho' => $Ho,
-            'ten' => $Ten,
-            'ngaysinh' => $NgaySinh,
-            'diachi' => $DiaChi,
-            'vitricongviec' => $ViTriCongViec
-        );
-        $arrData = loadRegistrations($fileName);
-        array_push($arrData, $contact);
-        $jsonData = json_encode($arrData, JSON_PRETTY_PRINT);
-        file_put_contents($fileName, $jsonData);
-}
-catch(Exception $e){
-    echo 'Lỗi: ', $e->getMessage(), "\n";
-}
-}
-$HoErr = null;
-$TenErr = null;
-$NgaySinhErr = null;
-$DiaChiErr = null;
-$ViTriCongViecErr = null;
-
-    if(  $_SERVER['REQUEST_METHOD'] == 'POST'){
-        $Ho = $_POST['Ho'];
-        $Ten = $_POST['Ten'];
-        $NgaySinh = $_POST['NgaySinh'];
-        $DiaChi = $_POST['DiaChi'];
-        $ViTriCongViec = $_POST['ViTriCongViec'];
-        $hasError = false;
-        if (empty($Ho)) {
-            $HoErr = "Họ không được để trống!";
-            $hasError = true;
-        }
-    
-        if (empty($Ten)) {
-            $TenErr = "Tên không được để trống!";
-            $hasError = true;
-        } 
-        
-    
-        if (empty($NgaySinh)) {
-            $NgaySinhErr = " Ngày sinh không được để trống!";
-            $hasError = true;
-        }
-        if (empty($DiaChi)) {
-            $DiaChiErr = " Địa chỉ không được để trống!";
-            $hasError = true;
-        }
-        if (empty($ViTriCongViec)) {
-            $ViTriCongViecErr = " Vị trí công việc không được để trống!";
-            $hasError = true;
-        }
-    
-        if (!$hasError) {
-            saveDataJSON("dulieu.json", $Ho, $Ten, $NgaySinh, $DiaChi, $ViTriCongViec);
-            $Ho = null;
-            $Ten = null;
-            $NgaySinh = null;
-            $DiaChi = null;
-            $ViTriCongViec = null;
-        }
-
-    }
-    if($Ho != '' && $Ten != '' && $NgaySinh != '' && $DiaChi != '' && $ViTriCongViec !=''){
-
-
+session_start();
 class NhanVien{
     public string $Ho;
     public string $Ten;
@@ -85,13 +14,14 @@ class NhanVien{
         $this->DiaChi = $DiaChi;
         $this->ViTriCongViec = $ViTriCongViec;
     }
-    public function setInfo(){
-        // $mang1($this->Ho,$this->Ten,$this->NgaySinh,$this->DiaChi,$this->ViTriCongViec);
-
-        // $this->mang[]=$mang1;
-    }
     public function getInfo(){
-        return $this->mang;
+        return [
+            "Ho" => $this->getHo(), 
+            "Ten" => $this->getTen(),
+            "NgaySinh"=>$this->getNgaySinh(),
+            "DiaChi"=>$this->getDiaChi(),
+            "ViTriCongViec"=>$this->getViTriCongViec()
+        ];
     }
     public function getHo(){
         return $this->Ho;
@@ -109,137 +39,149 @@ class NhanVien{
         return $this->ViTriCongViec;
     }
 }
-$Ten = new NhanVien($Ho,$Ten,$NgaySinh,$DiaChi,$ViTriCongViec);
 class QuanLy{
-    private array $Employee;
-    static array $ds;
+    private  $Employees =[];
     public function __construct()
     {
-        $this->Employee=[];
-        self::$ds=[];
+        if (isset($_SESSION["Employees"])){
+            $this->Employees = $_SESSION["Employees"];
+        }
     }
-    public function add($employees)///thêm nhân sự
+    public function add($NhanVien)///thêm nhân sự
     {
-        $this->Employee[]=$employees;
+        array_push($this->Employees, $NhanVien);
+        $_SESSION["Employees"] = $this->Employees;
     }
     public function getEmployee(): array///in ra chi tiết nhân sự
     {
-        return $this->Employee;
+        return $this->Employees;
     }
-
-    // public static $danhsach =[];
-
-    // public function hienthi(){
-        
+    // public function hienthi($ds1){
+    //     foreach( $ds1 as $key => $value){
+    //         foreach($value as $value){
+    //             echo $value."<br>";
+    //         }
+    //     }
     // }
-    // public function them(){
+    public function xemchitiet(){
 
-    // }
-    // public function xemchitiet(){
-
-    // }
-    // public function xoa(){
-
-    // }
-
-    // public function chinhsua(){
-
-    // }
+    }
+    public function xoa(){
+        unset($_SESSION['Employees']);
+    }
 }
 $Quanly1 = new Quanly();
-$Quanly1->add($Ten);
-// $Quanly1->add($Ten);'
-// $employee = $Quanly1->getEmployee();
+if(  $_SERVER['REQUEST_METHOD'] == 'POST'){
+    $Ho = $_POST['Ho'];
+    $Ten = $_POST['Ten'];
+    $NgaySinh = $_POST['NgaySinh'];
+    $DiaChi = $_POST['DiaChi'];
+    $ViTriCongViec = $_POST['ViTriCongViec'];
+
+    $NhanVien = new NhanVien($Ho,$Ten,$NgaySinh,$DiaChi,$ViTriCongViec);
+    $Thongtinnhanvien =  $NhanVien->getInfo();
+    
+    $Quanly1->add($Thongtinnhanvien);
+
+    // echo "<pre>";
+
+    // print_r ($Quanly1->getEmployee());
 }
+
+$employees = $Quanly1->getEmployee();
+
+
 ?>
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
     <style>
-        #date1 {
-            width: 97%;
-        }
-        .error{
-            color: #FF0000;
-        }
-        #table2 {
-            text-align: center ;
-            border-collapse: collapse;
-            width: 1500px;
-            border: solid 1px #ccc;
-        }
-        form {
-            width: 450px;
-        }
+    #date1 {
+        width: 97%;
+    }
+
+    .error {
+        color: #FF0000;
+    }
+
+    #table2 {
+        text-align: center;
+        border-collapse: collapse;
+        width: 1500px;
+        border: solid 1px #ccc;
+    }
+
+    form {
+        width: 450px;
+    }
     </style>
 </head>
+
 <body>
     <form action=" " method="post">
-    <fieldset>
-        <table>
-        <tr><td>Họ</td>
-        <td><input type="text" name="Ho" placeholder="Nhập Họ"></td>
-        <td><span class="error">* <?php echo $HoErr; ?></span><br></td>
-        </tr>
-        <tr><td>Tên</td>
-        <td><input type="text" name="Ten" placeholder="Nhập Tên"></td>
-        <td><span class="error">* <?php echo $TenErr; ?></span><br></td>
-        </tr>
-        <tr><td>Ngày Sinh</td>
-        <td><input type="date" id="date1" name="NgaySinh" placeholder="Nhập Ngày sinh"></td>
-        <td> <span class="error">* <?php echo $NgaySinhErr; ?></span><br> </td>
-        </tr>
-        <tr><td>Địa Chỉ</td>
-        <td><input type="text" name="DiaChi" placeholder="Nhập Địa Chỉ"></td>
-        <td> <span class="error">* <?php echo $DiaChiErr; ?></span><br> </td>
-        </tr>
-        <tr><td>Vị Trí Công Việc</td>
-        <td> <input type="text" name="ViTriCongViec" placeholder="Nhập Vị Trí Công Việc"></td>
-        <td><span class="error">* <?php echo $ViTriCongViecErr; ?></span>
-        </td>
-        </tr>
-        <tr><td></td>
-        <td><input type="submit" name="submit" value="Thêm"></td>
-        <td></td>
-        </tr>
-    </fieldset>
-    </table>
+        <fieldset>
+            <table>
+                <tr>
+                    <td>Họ</td>
+                    <td><input type="text" name="Ho" placeholder="Nhập Họ"></td>
+                </tr>
+                <tr>
+                    <td>Tên</td>
+                    <td><input type="text" name="Ten" placeholder="Nhập Tên"></td>
+                </tr>
+                <tr>
+                    <td>Ngày Sinh</td>
+                    <td><input type="date" id="date1" name="NgaySinh" placeholder="Nhập Ngày sinh"></td>
+                </tr>
+                <tr>
+                    <td>Địa Chỉ</td>
+                    <td><input type="text" name="DiaChi" placeholder="Nhập Địa Chỉ"></td>
+                </tr>
+                <tr>
+                    <td>Vị Trí Công Việc</td>
+                    <td> <input type="text" name="ViTriCongViec" placeholder="Nhập Vị Trí Công Việc"></td>
+                    </td>
+                </tr>
+                <tr>
+                    <td></td>
+                    <td><input type="submit" name="submit" value="Thêm"></td>
+                    <td></td>
+                </tr>
+        </fieldset>
+        </table>
     </form>
     <table id="table2">
-            <tr>
-                <td>ID</td>
-                <td width="100px">Họ</td>
-                <td width="100px">Tên</td>
-                <td width="100px">Ngày Sinh</td>
-                <td width="550px">Địa Chỉ</td>
-                <td width="350px">Công việc</td>
-                <td>Thao tác</td>
-            </tr>
-            <?php
-            $employee = loadRegistrations('dulieu.json');
-            if ($employee != '') {
-             foreach ($employee as $key => $registration){  ?>
-            <tr>
-                <td><?php echo $key ?></td>
-                <td><?= $registration['ho']; ?></td>
-                <td><?= $registration['ten'];  ?></td>
-                <td><?= $registration['ngaysinh'];  ?></td>
-                <td><?= $registration['diachi'];  ?></td>
-                <td><?= $registration['vitricongviec']; ?></td>
-                <td>
-                    <a href="show.php?id=<?php echo $key; ?>">Show</a>
-                    <a href="edit.php?id=<?php echo $key; ?>">Edit</a>
-                    <a href="edit.php?id=<?php echo $key; ?>">Delete</a>
-                </td>
-            </tr>
+        <tr>
+            <td>ID</td>
+            <td width="100px">Họ</td>
+            <td width="100px">Tên</td>
+            <td width="100px">Ngày Sinh</td>
+            <td width="550px">Địa Chỉ</td>
+            <td width="350px">Công việc</td>
+            <td>Thao tác</td>
+        </tr>
+        <?php
+            if ($employees != '') {
+                // echo "<pre>";
+                // print_r($employees);
+             foreach ($employees as $key => $value){ ?>
+        <tr>
+            <td><?php echo $key +1 ?></td>
+            <td><?php echo $value["Ho"];?></td>
+            <td><?php echo $value["Ten"];?></td>
+            <td><?php echo $value["NgaySinh"];?></td>
+            <td><?php echo $value["DiaChi"];?></td>
+            <td><?php echo $value["ViTriCongViec"];?></td>
+            <td></td>
+        </tr>
         <?php } } ?>
-        </table>
- 
-</body>
-</html>
+    </table>
 
-       
+</body>
+
+</html>
