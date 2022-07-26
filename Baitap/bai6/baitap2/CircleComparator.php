67f29c6d633cc3f1db7c8f666cc223483642256b
MaiXuanCuong <maixuancuong2906@gmail.com>
2022-07-26 15:52:14 +0700

    testtest

---
+++ Baitap/bai6/baitap2/CircleComparator.php
@@ -0,0 +1,19 @@
+<?php
+include_once(dirname(__FILE__) . '/../baitap1/Circle.php');
+include "Comparator.php";
+class CircleComparator implements Comparator
+{
+    public function compare(object $circleOne, object $circleTwo): int
+    {
+        $radiusOne = $circleOne->getRadius();
+        $radiusTwo = $circleTwo->getRadius();
+
+        if ($radiusOne > $radiusTwo) {
+            return 1;
+        } else if ($radiusOne < $radiusTwo) {
+            return -1;
+        } else {
+            return 0;
+        }
+    }
+}
