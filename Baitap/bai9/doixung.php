dfff763967a5bec0d29379fbb04d456da6b72340
MaiXuanCuong <maixuancuong2906@gmail.com>
2022-08-02 08:16:49 +0700

    testtest

--- Baitap/bai9/doixung.php
+++ Baitap/bai9/doixung.php
@@ -3,28 +3,24 @@ class Stack{
     public $doixung = [] ;
     public $stack= [];
     public $queue=[];
-    public function __construct($stack, $queue) {
-       $this->stack=  str_split($stack);
-       
+    public function __construct($queue) {
        $this->queue = str_split($queue); 
-       
+       $this->stack = str_split($queue); 
     }
     public function compare(){
         $check =true;
-        while (count($this->stack) > 0 && $check == true) {
-          $last = array_pop($this->stack);
-          array_unshift($this->doixung, $last);
-          $fisrt = array_shift($this->queue);
-          array_push($this->doixung, $fisrt);
+        while (count($this->queue) > 0 && $check == true) {
+        $last = array_pop($this->stack);
+        array_unshift($this->doixung, $last);
+        $fisrt = array_shift($this->queue);
+        array_push($this->doixung, $fisrt);
           if ($last === $fisrt) {
-            $check = true;
-          }else {
-            $check = false;
-          }
+                $check = true;
+                }else{
+                    $check = false;
+            } 
         }
         return $check;
-       
-     
     }
     public function check(){
         if ( $this->compare()) {
@@ -35,9 +31,8 @@ class Stack{
         }
     }
 }
-$stack = "hi";
-$queue = "hi";
-$kiemtra = new Stack($stack,$queue);
+$queue = "elba11abl1e";
+$kiemtra = new Stack($queue);
 $kiemtra->compare();
 $kiemtra->check();
 echo "<pre>";
