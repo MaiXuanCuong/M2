930fdf070ad226fedf43c793e1776a4d10abd39e
MaiXuanCuong <maixuancuong2906@gmail.com>
2022-08-02 17:22:28 +0700

    testtest

---
+++ Baitap/bai8/baitap/trienkhailopLinkedList.php
@@ -0,0 +1,50 @@
+<?php
+class Node {
+    public $elements;
+    public $next;
+    public function __construct($elements){
+        $this->elements = $elements;
+        $this->next = null;
+    }
+}
+class LinkedList{
+    public $count = 0;
+    public $firstNode;
+    public $lastNode;
+    public function __construct() {
+
+    }
+    public function add(int $index) {
+    
+    }   
+    public function addFirst(int $index) { 
+
+    }
+    public function addLast(int $index) {
+
+    }
+    public function remove(int $index) {
+
+    }
+    public function get(int $index) {
+
+    }
+    public function size()  {
+        
+    }
+    public function printList() {
+
+    }
+    public function clone() {
+
+    }
+    public function contains(){
+
+    }
+    public function indexOf(){
+
+    }
+}
+
+
+?>
