5e830441793e544bc1af410df5dde5fa336700a0
MaiXuanCuong <maixuancuong2906@gmail.com>
2022-08-04 14:07:21 +0700

    testtest

--- Baitap/bai12/sapxepluachon.php
+++ Baitap/bai12/sapxepluachon.php
@@ -1,25 +1,30 @@
 <?php
-$array = [1, 9, 4.5, 6.6, 5.7, -4.5];
-$count = count($array);
-echo "<pre>";
-print_r($array);
-for ($i = 0; $i < $count; $i++){
-    $min = $i;
-    for ($j = $i+1; $j < $count; $j++){
-        if ($array[$j] < $array[$min]){
-            $min = $j;
+
+function selectionSort($list,$count){
+    for ($i = 0; $i < $count; $i++){
+        $min = $i;
+        for ($j = $i+1; $j < $count; $j++){
+            if ($list[$j] < $list[$min]){
+                $min = $j;
+            }
+    
         }
-
+        if ($min != $i){
+            $temp = $list[$min];
+            $list[$min] = $list[$i];
+            $list[$i] = $temp;
+        }
+    
     }
-    if ($min != $i){
-        $temp = $array[$min];
-        $array[$min] = $array[$i];
-        $array[$i] = $temp;
-    }
-
+    return $list;
 }
+$list = [1, 9, 4.5, 6.6, 5.7, -4.5];
+$count = count($list);
 echo "<pre>";
-print_r($array);
+print_r($list);
+
+echo "<pre>";
+print_r(selectionSort($list,$count));
 
 
 ?>
