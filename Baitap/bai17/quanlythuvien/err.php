dd3dbc57729028e1f095ce21bc3f790468e6c490
MaiXuanCuong <maixuancuong2906@gmail.com>
2022-08-25 11:02:25 +0700

    testtest

--- Baitap/bai17/quanlythuvien/err.php
+++ Baitap/bai17/quanlythuvien/err.php
@@ -5,6 +5,10 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-0evHe/X+R7YkIZDRvuzKMRqM+OrBnVFBL6DOitfPri4tjfHxaWutUpFmBp4vmVor" crossorigin="anonymous">
+        <!-- <link href="https://cdn.jsdelivr.net/npm/simple-datatables@latest/dist/style.css" rel="stylesheet" /> -->
+        <!-- <link href="../css/styles.css" rel="stylesheet" /> -->
+        <script src="https://use.fontawesome.com/releases/v6.1.0/js/all.js" crossorigin="anonymous"></script>
     <style>
         h1{
             font-size: 700%;
@@ -17,11 +21,12 @@
         h3{
             text-align: center;
         }
+
     </style>
 </head>
 <body>
 <h2>OOPS!</h2>
     <h1>404</h1>
-  <h3><a class="err" href="index/index.php">Trở về Trang chủ</a></h3>
+  <h3><a class="btn btn-primary" href="index/index.php">Trở về Trang chủ</a></h3>
 </body>
 </html>
