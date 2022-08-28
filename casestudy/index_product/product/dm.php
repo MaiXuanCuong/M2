6ff62bcaf3e2f9589a08c3e34230862f1f55b494
MaiXuanCuong <maixuancuong2906@gmail.com>
2022-08-28 18:18:31 +0700

    testtest

--- casestudy/index_product/product/dm.php
+++ casestudy/index_product/product/dm.php
@@ -3,6 +3,7 @@ include_once "layout/header.php";
 include_once "layout/sidebar.php";?>
 <?php 
 global $conn;
+$id = $_REQUEST['id'];
 $sql = "SELECT * FROM `product` JOIN categories 
 ON product.category_id = categories.id_category";
 $stmt = $conn->query($sql);
@@ -20,28 +21,28 @@ $rows = $stmt->fetchAll();
         <div class="col-xl-3 col-md-6">
                 <div style="border-radius: 10px/10px" class="card  text-white mb-4">
                     <div class=" d-flex align-items-center justify-content-between">
-                        <a class="small text-white stretched-link" href="product/cate.php"><img width="290px" height="50px" src="../product/image/hang1_1629782045.png"></a>
+                        <a class="small text-white stretched-link" href="product/category/index.php"><img width="290px" height="50px" src="../product/image/hang1_1629782045.png"></a>
                     </div>
                 </div>
             </div>
             <div class="col-xl-3 col-md-6">
                 <div style="border-radius: 10px/10px" class="card  text-white mb-4">
                     <div class=" d-flex align-items-center justify-content-between">
-                        <a class="small text-white stretched-link" href="product/cate.php"><img width="290px" height="50px" src="../product/image/hang2_1629782097.png"></a>
+                        <a class="small text-white stretched-link" href="product/category/index.php"><img width="290px" height="50px" src="../product/image/hang2_1629782097.png"></a>
                     </div>
                 </div>
             </div>
             <div class="col-xl-3 col-md-6">
                 <div style="border-radius: 10px/10px" class="card  text-white mb-4">
                     <div class="d-flex align-items-center justify-content-between">
-                        <a class="small text-white stretched-link" href="product/cate.php"><img width="290px" height="50px" src="../product/image/hang3_1629782127.png"></a>
+                        <a class="small text-white stretched-link" href="product/category/index.php"><img width="290px" height="50px" src="../product/image/hang3_1629782127.png"></a>
                     </div>
                 </div>
             </div>
             <div class="col-xl-3 col-md-6">
                 <div style="border-radius: 10px/10px" class="card  text-white mb-4">
                     <div class=" d-flex align-items-center justify-content-between">
-                        <a class="small text-white stretched-link" href="product/cate.php"><img width="290px" height="50px" src="../product/image/hang4_1629782113.png"></a>
+                        <a class="small text-white stretched-link" href="product/category/index.php"><img width="290px" height="50px" src="../product/image/hang4_1629782113.png"></a>
                     </div>
                 </div>
             </div>
@@ -57,7 +58,7 @@ $rows = $stmt->fetchAll();
                     </div>
                         <div style="text-align: center">
                             <img width="300px" height="330px" src="./../product/image/<?php echo $value->image?>"/><br>
-                            <a href="product/dm.php?id=<?php echo $value->id_product?>"><?php echo $value->name_product?></a><br>
+                            <a href="product/index.php?id=<?php echo $value->id_product?>"><?php echo $value->name_product?></a><br>
                             <p><?php echo number_format($value->price)." VNĐ"?></p>
                         </div>
                 </div>
