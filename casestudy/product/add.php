80e6b9a58d75b83c0a9a96c7db16068866d66f51
MaiXuanCuong <maixuancuong2906@gmail.com>
2022-08-28 20:38:22 +0700

    testtest

--- casestudy/product/add.php
+++ casestudy/product/add.php
@@ -41,7 +41,7 @@ if(empty($err))
     $sql = "INSERT INTO `product` 
             (`name_product`,`price`,`category_id`,`describe`,`quantity`,`specifications`,`image`,`color`) 
             VALUES 
-            ('$name','$price','$category','$describe','$quantity','$specifications','$image',$color)";
+            ('$name','$price','$category','$describe','$quantity','$specifications','$image','$color')";
     $conn->exec($sql);
     header('location:../index/index.php');
 }
@@ -62,6 +62,7 @@ if(empty($err))
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-0evHe/X+R7YkIZDRvuzKMRqM+OrBnVFBL6DOitfPri4tjfHxaWutUpFmBp4vmVor" crossorigin="anonymous">
 </head>
 <body>
+    <br>
 <div id="layoutSidenav_content">
         <main>
     <div class="container">
