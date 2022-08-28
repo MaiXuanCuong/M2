d23fcc94793c263a4c3cf0f7ad8dfda34427f5c1
MaiXuanCuong <maixuancuong2906@gmail.com>
2022-08-28 17:41:20 +0700

    testtest

--- casestudy/product/delete.php
+++ casestudy/product/delete.php
@@ -2,7 +2,7 @@
 include_once '../database.php';
 $id = $_REQUEST['id'];
 if (isset($_REQUEST['id'])){
-$sql = "DELETE FROM `books` WHERE `id_book` = $id ";
+$sql = "DELETE FROM `product` WHERE `id_product` = $id ";
 try {
     $conn->exec($sql);
  header("location:index.php");
